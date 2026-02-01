d6d8923472e61cbb92a5106ab887b9004ca5ee5d
Felipe <felipelangabtc@outlook.com>
2026-02-01 20:03:16 +1000

    fix: remove duplicate exports in components index

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -5,12 +5,3 @@ export { SearchBar } from './SearchBar';
 export type { SearchFilters } from './SearchBar';
 
 export { AuthForm } from './AuthForm';
-
-export { default as PropertyCard } from './PropertyCard';
-export { default as SearchBar } from './SearchBar';
-export { default as AuthForm } from './AuthForm';
-
-// Re-export all components for convenience
-export * from './PropertyCard';
-export * from './SearchBar';
-export * from './AuthForm';
